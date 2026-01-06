c1668edbd6cce2eb057d15202bf9c9f992195ff9
Barry Liu <barry.y.liu@hotmail.com>
2026-01-06 19:04:19 +0800

    [U] Optimization

--- constants/framework.py
+++ constants/framework.py
@@ -6,6 +6,13 @@ from enum import Enum, unique
 
 
 @unique
-class Framework(Enum):
+class Deployment(Enum):
     DEPLOYMENT_IN_DIFFERENT_REGION = 1
     DEPLOYMENT_IN_SAME_REGION = 2
+
+
+@unique
+class ExecuteMode(Enum):
+    LOCAL = "local_for_all"
+    SQS = "local_for_sqs"
+    AWS_LAMBDA = "local_for_aws_lambda"
