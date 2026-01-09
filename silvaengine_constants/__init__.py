4ff8728f645ca99ce83a5f27620aa030cc3a64f3
Barry Liu <barry.y.liu@hotmail.com>
2026-01-09 22:52:27 +0800

    [U] Optimization

--- silvaengine_constants/__init__.py
+++ silvaengine_constants/__init__.py
@@ -2,11 +2,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 
-from .framework import Deployment, ExecuteMode
+from .aws import EventType
+from .framework import AuthorizationType, Deployment, ExecuteMode, RequestMethod
 from .http import HttpStatus
 
 __all__ = [
     "HttpStatus",
     "Deployment",
     "ExecuteMode",
+    "AuthorizationType",
+    "RequestMethod",
+    "EventType",
 ]
--- silvaengine_constants/framework.py
+++ silvaengine_constants/framework.py
@@ -16,3 +16,19 @@ class ExecuteMode(Enum):
     LOCAL = "local_for_all"
     SQS = "local_for_sqs"
     AWS_LAMBDA = "local_for_aws_lambda"
+
+
+@unique
+class AuthorizationType(Enum):
+    REQUEST = 1
+    TOKEN = 2
+
+
+@unique
+class RequestMethod(Enum):
+    GET = 1
+    POST = 2
+    PUT = 3
+    PATCH = 4
+    DELETE = 5
+    HEAD = 6
